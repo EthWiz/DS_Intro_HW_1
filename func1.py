f8dce8040a35985180d2ee262ad68a55abd7a370
aviv345 <ami.ruskin@gmail.com>
2022-03-04 11:42:32 +0200

    started matala

---
+++ func1.py
@@ -0,0 +1,9 @@
+def my_func(x1, x2, x3):
+    sum1=𝒙𝟏+𝒙𝟐+𝒙𝟑
+    sum2=𝒙𝟐+𝒙𝟑
+    mone=sum1*sum2*x3
+    mechane=𝒙𝟏+𝒙𝟐+𝒙𝟑
+    if mechane == 0:
+        return 'Not a number – denominator equals zero'
+    else
+
