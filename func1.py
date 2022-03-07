37bff2e8436513f0c04c700bb60d696fd98f7545
aviv345 <ami.ruskin@gmail.com>
2022-03-07 16:50:18 +0200

    fixed func 1

--- func1.py
+++ func1.py
@@ -1,9 +1,16 @@
 def my_func(x1, x2, x3):
-    sum1=𝒙𝟏+𝒙𝟐+𝒙𝟑
-    sum2=𝒙𝟐+𝒙𝟑
-    mone=sum1*sum2*x3
-    mechane=𝒙𝟏+𝒙𝟐+𝒙𝟑
-    if mechane == 0:
-        return 'Not a number – denominator equals zero'
-    else
+    try:
+        sum1=𝒙𝟏+𝒙𝟐+𝒙𝟑
+        sum2=𝒙𝟐+𝒙𝟑
+        mone=sum1*sum2*x3
+        mechane=𝒙𝟏+𝒙𝟐+𝒙𝟑
+        calculate= mone/mechane
+        if mechane == 0:
+            return 'Not a number – denominator equals zero'
+        else:
+            return calculate
+    except:
+        return 'Error: parameters should be float'
+
+print(my_func("want",3.2,8884.1))
 
